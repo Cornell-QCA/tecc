f7ddf32cca9eb783c048a3d3c8c1da6b99a3f679
Haadi Khan <haadikhan05@gmail.com>
2025-03-06 23:45:27 -0500

    Add LatticeManager — main boilerplate for Harrington impl
    Basically all the Harrington rules will be implemented through
    LatticeManager (at least the way I see it right now). Broadcasting and
    all other roles can probably be handled through it. Maybe this should be
    refactored at some point, but I think this is the most straightforward
    impl to start with.

--- harrington/src/lattice.rs
+++ harrington/src/lattice.rs
@@ -1,21 +1,78 @@
 use crate::automaton::Automaton;
-use slotmap::{SlotMap, basic};
+use slotmap::SlotMap;
 use std::collections::HashMap;
-
-slotmap::new_key_type! { struct QubitKey; }
 
 enum LatticeType {
     Primary,
     Dual,
 }
 
-// The graph owns Automatons and (vecs of) EdgeKeys
-pub struct Lattice<'clock, 'automata_ref> {
-    colony_size: u32,
+slotmap::new_key_type! {
+    pub struct LatticeId;
+}
+
+slotmap::new_key_type! {
+    struct QubitKey;
+}
+
+/// LatticeManager is the "higher level" lattice structure.
+pub struct LatticeManager<'clock, 'automata_ref> {
     work_period: u32,
+    clock: u32, // will send a reference w/ 'clock to automata
+    colony_size: u32,
+    colony_degree: u32, // `k` in the paper
     cell_threshold: f64,
     neighbor_threshold: f64,
-    clock: u32, // will send a reference w/ 'clock to automata
+
+    store: SlotMap<LatticeId, Lattice<'clock, 'automata_ref>>,
+}
+
+impl<'clock, 'automata_ref> LatticeManager<'clock, 'automata_ref> {
+    pub fn new(
+        colony_size: u32,
+        colony_degree: u32,
+        work_period: u32,
+        cell_threshold: f64,
+        neighbor_threshold: f64,
+    ) -> Self {
+        let store: SlotMap<LatticeId, Lattice<'clock, 'automata_ref>> = SlotMap::with_key();
+
+        // TODO: Build the lattice and add it to the store.
+
+        Self {
+            work_period,
+            clock: 0,
+            colony_size,
+            colony_degree,
+            cell_threshold,
+            neighbor_threshold,
+            store,
+        }
+    }
+
+    // Example method for building new lattice
+    pub fn _create_lattice(&mut self, _supercolony: Option<LatticeId>) -> LatticeId {
+        todo!();
+        // let id = self.store.insert(Lattice {
+        //     supercolony,
+        //     colonies: vec![],
+        // });
+        // if let Some(pid) = supercolony {
+        //     self.store[pid].colonies.push(id);
+        // }
+        // id
+    }
+
+    // Example method for colony communication
+    pub fn _broadcast_to_supercolony(&mut self, _me: LatticeId, _msg: &str) {
+        todo!()
+    }
+}
+
+pub struct Lattice<'clock, 'automata_ref> {
+    supercolony: Option<LatticeId>,
+    colonies: Vec<LatticeId>,
+
     primary: HashMap<Automaton<'clock>, Vec<Edge<'automata_ref, 'clock>>>,
     dual: HashMap<Automaton<'clock>, Vec<Edge<'automata_ref, 'clock>>>,
 }
@@ -31,8 +88,3 @@ pub struct LatticeEdge<'automata, 'clock> {
 }
 
 impl<'automata, 'clock> Edge<'automata, 'clock> {}
-
-// // If we want this to be thread-safe, we should use Mutex instead of RefCell and check SlotMap
-// // the qubits are owned by a slotmap
-// let mut qubits: SlotMap<QubitKey, RefCell<Qubit>> = SlotMap::new();
-// let mut edges: SlotMap<EdgeKey, RefCell<Qubit>> = SlotMap::new();
